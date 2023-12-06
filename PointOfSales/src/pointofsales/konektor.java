0dd1691b8459ad30522e261b6d0605b4fb1f5db5
dianfitrii <dianutamiii00@gmail.com>
2023-12-06 19:30:46 +0700

    aa

---
+++ PointOfSales/src/pointofsales/konektor.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pointofsales;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+/**
+ *
+ * @author ACER
+ */
+public class konektor {
+    static Connection mysqlconfig;
+
+    public static Connection koneksi() {
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/phpmyadmin";
+            String user = "root";
+            String pass = "";
+            
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
+            return mysqlconfig;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Tidak dapat terhubung ke Database: " + e.getMessage());
+            throw new RuntimeException("Tidak dapat terhubung ke Database", e);
+        }
+    }
+    
+    public ResultSet getResultData(String query) {
+        try {
+            Connection connection = koneksi();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            return preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
